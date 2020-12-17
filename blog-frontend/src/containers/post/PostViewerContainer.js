9bbfa2d8a6a3b87869d8dc5f7ddaddee1c36f3e2
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-12-17 19:33:42 +0900

    포스트 삭제

--- blog-frontend/src/containers/post/PostViewerContainer.js
+++ blog-frontend/src/containers/post/PostViewerContainer.js
@@ -5,6 +5,7 @@ import { readPost, unloadPost } from '../../modules/post';
 import PostViewer from '../../components/post/PostViewer';
 import PostActionButtons from '../../components/post/PostActionButtons';
 import { setOriginalPost } from '../../modules/write';
+import { removePost } from '../../lib/api/posts';
 
 const PostViewerContainer = ({ match, history }) => {
   const { postId } = match.params;
@@ -24,6 +25,14 @@ const PostViewerContainer = ({ match, history }) => {
     history.push('/write');
   };
 
+  const onRemove = async () => {
+    try {
+      await removePost(postId);
+      history.push('/');
+    } catch (e) {
+      console.log(e);
+    }
+  };
   const ownPost = (user && user._id) === (post && post.user._id);
 
   // 언마운트될 때 초기화
@@ -40,7 +49,9 @@ const PostViewerContainer = ({ match, history }) => {
       post={post}
       error={error}
       loading={loading}
-      actionButtons={ownPost && <PostActionButtons onEdit={onEdit} />}
+      actionButtons={
+        ownPost && <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+      }
     />
   );
 };
